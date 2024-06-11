6d151354379fc453e6e26c65fef9afbfd33b298c
Artur Assis Alves <arturassiscomp@gmail.com>
2024-06-11 16:18:37 -0300

    add tests for getRawHours

--- test/stop_watch_timer_test.dart
+++ test/stop_watch_timer_test.dart
@@ -2,6 +2,9 @@ import 'package:stop_watch_timer/stop_watch_timer.dart';
 import 'package:test/test.dart';
 
 void main() {
+  // Max int that can be represented in any platform, including the web, without
+  // losing precision.
+  const maxInt = 9007199254740992; // 2 ^ 53
   group('Class: StopWatchTimer', () {
     group('Constructor: unnamed', () {
       test('Should have default values', () {
@@ -47,6 +50,42 @@ void main() {
         expect(stopWatchTimerWithDefaults.onStopped, onStopped);
         expect(stopWatchTimerWithDefaults.onEnded, onEnded);
         expect(stopWatchTimerWithDefaults.initialPresetTime, presetMillisecond);
+      });
+    });
+    group('Static methods', () {
+      group('Method: getRawHours', () {
+        const msKey = 'msKey';
+        const expectedHoursKey = 'expectedHoursKey';
+        const oneHourInMilliseconds = 1000 * 60 * 60;
+        const threeHoursInMilliseconds = 3 * 1000 * 60 * 60;
+        const maxHoursInMilliseconds = maxInt;
+        const maxHours =
+            2501999792; // 9007199254740992 ~/ oneHourInMilliseconds
+
+        final testCases = <Map<String, int>>[
+          {msKey: 0, expectedHoursKey: 0},
+          {msKey: 1, expectedHoursKey: 0},
+          {msKey: 1234, expectedHoursKey: 0},
+          {msKey: oneHourInMilliseconds - 1, expectedHoursKey: 0},
+          {msKey: oneHourInMilliseconds, expectedHoursKey: 1},
+          {msKey: oneHourInMilliseconds + 1, expectedHoursKey: 1},
+          {msKey: threeHoursInMilliseconds - 1, expectedHoursKey: 2},
+          {msKey: threeHoursInMilliseconds, expectedHoursKey: 3},
+          {msKey: threeHoursInMilliseconds + 1, expectedHoursKey: 3},
+          {msKey: maxHoursInMilliseconds - 1, expectedHoursKey: maxHours},
+          {msKey: maxHoursInMilliseconds, expectedHoursKey: maxHours},
+        ];
+        for (final item in testCases) {
+          final milliSecond = item[msKey]!;
+          final expectedRawHours = item[expectedHoursKey]!;
+          test(
+            'Should return $expectedRawHours h for input $milliSecond ms',
+            () => expect(
+              StopWatchTimer.getRawHours(milliSecond),
+              equals(expectedRawHours),
+            ),
+          );
+        }
       });
     });
   });
